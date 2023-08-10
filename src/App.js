366170ff97e2b9807bdbe274c20388888bc4b3ce
chansukman <13060941803@163.com>
2023-08-10 11:27:31 +0100

    The first layout for clone chatgpt

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+const App = () =>{
+  return (
+    <div className="App">
+      <section className='side-bar'>
+        <button className="newChatBtn">+ New Chat</button>
+        <ul className='history'>
+          <li>dkdkdk</li>
+        </ul>
+        <nav className="leftNav">
+          <p>Made by Stephanie</p>
+        </nav>
+      </section>
+      <section className='main'></section>
+    </div>
+  );
+}
+
+export default App;
